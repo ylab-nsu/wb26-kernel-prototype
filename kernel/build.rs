c7057fb9b1515a64936a2cfe091f8dabc38ea36c
Timofey X <vuki03@mail.ru>
2026-04-01 16:15:41 +0700

    Fix differences in user-programs package and build configuration
    Also, previous commit version would not build after clean. And it wouldn't support release profile

--- kernel/build.rs
+++ kernel/build.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
+const USER_PACKAGE: &str = "user-programs";
+
 fn main() {
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let workspace_root = manifest_dir.parent().unwrap();
@@ -12,17 +14,16 @@ fn main() {
     println!("cargo:rustc-link-arg=--allow-multiple-definition");
     println!("cargo:rerun-if-changed=link_script.ld");
 
-    let user_target_dir = &workspace_root.join("target/user-programs");
-    let mut user_cargo_args = vec![
-        "build",
-        "--package", "user-processes",
-        "--target", &target,
-    ];
-    match env::var("PROFILE").as_deref() {
-        Ok("debug") => {}
-        Ok("release") => {user_cargo_args.push("--release");}
-        Ok(p) => {panic!("Unknown profile: {p}")}
-        Err(e) => {panic!("{e}")}
+    let user_target_dir = &workspace_root.join("target").join(USER_PACKAGE);
+    let mut user_cargo_args = vec!["build", "--package", USER_PACKAGE, "--target", &target];
+
+    let profile = env::var("PROFILE").unwrap();
+    match profile.as_str() {
+        "debug" => {}
+        "release" => user_cargo_args.push("--release"),
+        p => {
+            panic!("Unknown profile: {p}")
+        }
     }
 
     let status = Command::new("cargo")
@@ -31,16 +32,17 @@ fn main() {
         .status();
 
     if status.is_err() || !status.unwrap().success() {
-        panic!("Failed to build user-processes module");
+        panic!("Failed to build {USER_PACKAGE} package");
     }
 
     let workspace_root = manifest_dir.parent().unwrap();
     let app_elf = workspace_root
         .join(user_target_dir)
         .join(&target)
-        .join("debug/libuser.a");
+        .join(profile)
+        .join(format!("lib{}.a", USER_PACKAGE.replace("-", "_")));
 
     println!("cargo:rustc-link-arg={}", app_elf.display());
-    println!("cargo:rerun-if-changed=../user-processes", );
+    println!("cargo:rerun-if-changed=../{USER_PACKAGE}",);
     println!("cargo:rerun-if-changed=build.rs");
 }
