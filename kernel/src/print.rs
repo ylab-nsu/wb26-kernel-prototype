7a0270801cfe3e2d5039d8dcfc163e3da752ab79
Nikolay Repin <nikolat-repin201303@mail.ru>
2026-04-14 04:45:19 +0700

    Implement simple log-like macros

--- kernel/src/print.rs
+++ kernel/src/print.rs
@@ -13,3 +13,37 @@ macro_rules! println {
         writeln!($crate::arch::DebugWriter::new(), $($arg)*).ok();
     }};
 }
+
+macro_rules! rich_println {
+    ($prefix:tt, $($arg:tt)*) => {
+        println!("{:8} {}:{}:{} - {}", $prefix, file!(), line!(), column!(), format_args!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {
+        rich_println!("[INFO]", $($arg)*);
+    };
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {
+        rich_println!("[WARN]", $($arg)*);
+    };
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {
+        rich_println!("[ERROR]", $($arg)*);
+    };
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        rich_println!("[DEBUG]", $($arg)*);
+    };
+}
