2f80292032cf178d5b365b0d87e920a8965e5182
Nikolay Repin <nikolat-repin201303@mail.ru>
2026-06-09 04:15:47 +0700

    Fix `LazyLock` type parameters

--- kernel/src/sync.rs
+++ kernel/src/sync.rs
@@ -3,5 +3,5 @@
 pub type Mutex<T> = spin::mutex::Mutex<T, spin::relax::Spin>;
 pub type RwLock<T> = spin::rwlock::RwLock<T, spin::relax::Spin>;
 pub type Once<T> = spin::once::Once<T, spin::relax::Spin>;
-pub type LazyLock<T> = spin::lazylock::LazyLock<T, spin::relax::Spin>;
+pub type LazyLock<T> = spin::lazylock::LazyLock<T, fn() -> T, spin::relax::Spin>;
 pub type Barrier = spin::barrier::Barrier<spin::relax::Spin>;
